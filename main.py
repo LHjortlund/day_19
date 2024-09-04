dc9f698aa718b70638a6acf7bf6c79bcb5c10243
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-04 13:59:17 +0200

    press a key and then draw the line = Event listener

--- main.py
+++ main.py
@@ -7,5 +7,22 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def move_counter_clockwise():
+    tim.left(45)
+    tim.heading() +10
+
+def move_clockwise():
+    tim.right(45)
+    tim.heading() +10
+
 screen.listen()
-screen.onkey()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=move_forwards)
+
+screen.exitonclick()
